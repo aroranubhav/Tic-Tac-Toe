38d82405fa201d3252a5e174bf2c3b795a3f746b
maxi <arora.anubhav93@gmail.com>
2025-09-02 23:16:01 +0530

    Add constants and string resources

---
+++ app/src/main/java/com/maxi/tic_tac_toe/util/Constants.kt
@@ -0,0 +1,12 @@
+package com.maxi.tic_tac_toe.util
+
+object Constants {
+
+    const val DEFAULT_BOARD_SIZE = 3
+    const val PLAYER_X = "X"
+    const val PLAYER_O = "O"
+    const val OUT_OF_BOUND_MOVE = "Invalid move: out of bounds."
+    const val ALREADY_OCCUPIED_CELL_MOVE = "Invalid move: cell is already occupied."
+    const val FORWARD = "forward"
+    const val BACKWARD = "backward"
+}
